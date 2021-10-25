4934d242f39e5de7ec4dc03dcbf9a3a2610a259b
arieadityanugraha <arieadityan@gmail.com>
2021-10-25 22:04:08 +0700

    style(component): update 404 page

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { Fragment } from "react";
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Content from "../../assets/404.png"
+import "./ErrorStyle.scss";
 
-function Error() {
-  return <h1>Page not found!</h1>
+const Error = () => {
+
+  return (
+    <Fragment> 
+        <div className="error-wrapper">
+            <img src={Content} alt="404-page" width="500"/>
+            <h1 className="error-title">Oh, no! This page does not exist</h1>
+            <Link to="/">
+              <Button className="btn-back">Go to main page</Button>
+            </Link>
+        </div>
+    </Fragment>
+  )
+  
 };
 
 export default Error;
