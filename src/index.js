9b546e8e333321b816ada6f6af1c71c62a4e681e
arieadityanugraha <arieadityan@gmail.com>
2021-10-25 23:30:29 +0700

    feat(config): add service worker for web caching

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 
 ReactDOM.render(
@@ -11,6 +12,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
