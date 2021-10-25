dd921f14b7c4573d0a7586cc776d178fc2eafa14
arieadityanugraha <arieadityan@gmail.com>
2021-10-25 22:06:02 +0700

    feat(config): redirect to main page after submit data

--- src/components/FormAdd/FormAdd.js
+++ src/components/FormAdd/FormAdd.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { 
     Container,
     Alert
@@ -7,33 +8,40 @@ import JsonToForm from 'json-reactform';
 import Schema from './Schema';
 import SteinStore from "stein-js-client";
 import "./FormAddStyles.scss";
+import Header from "../Header/Header";
 
 const FormAdd = () => {
+    const [isSubmit, setIsSubmit] = useState(false);
 
     const store = new SteinStore(
         "https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4"
     );
+
+    const history = useHistory();
    
     const submit = (params) => {
-
+        setIsSubmit(true);
         store.append("list", [{
-                uuid: params.uuid,
-                komoditas: params.komoditas,
-                area_provinsi: params.area_provinsi.value,
-                area_kota: params.area_kota.value,
-                size: params.size,
-                price: params.price,
-                tgl_parsed: params.tgl_parsed,
-                timestamp: params.timestamp
-            }]).then(res => {
-                console.log(res);
-            });
+            uuid: params.uuid,
+            komoditas: params.komoditas,
+            area_provinsi: params.area_provinsi.value,
+            area_kota: params.area_kota.value,
+            size: params.size,
+            price: params.price,
+            tgl_parsed: params.tgl_parsed,
+            timestamp: params.timestamp
+        }]).then(res => {
+            console.log(res);
+            history.push("/");
+        }, 500);
+
     }
 
     return (
         <Container className="wrapper-form">
+            <Header/>
             <h1>Tambah Data</h1>
-            <Alert variant="success" dismissible>Data berhasil ditambahkan</Alert>
+            {isSubmit && <Alert variant="success" dismissible>Data berhasil ditambahkan</Alert> }
             <JsonToForm model={Schema} onSubmit={submit}/>
         </Container>
         
